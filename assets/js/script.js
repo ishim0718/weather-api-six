ae5add641902b04914a22b1726489667519f84be
ishim0718 <ishim0718@gmail.com>
2023-03-07 22:14:40 -0500

    added index

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var location = document.querySelector("#location");
+var icon = document.querySelector("#icon");
+var date = document.querySelector("#date");
+var temperature = document.querySelector("#temp");
+var wind = document.querySelector("#wind");
+var humidity = document.querySelector("#humidity");
+
+
+var api = "339911761f464aefd97177f16c15c048";
+
